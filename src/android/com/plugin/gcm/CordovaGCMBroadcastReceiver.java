0b3c4f3b88e60abe402c22a99790d5756e53ed7b
evergreen-lee-campbell <lee.campbell@evergreen-life.co.uk>
2018-05-21 09:43:54 +0100

    Included bitmap references

--- src/android/com/plugin/gcm/CordovaGCMBroadcastReceiver.java
+++ src/android/com/plugin/gcm/CordovaGCMBroadcastReceiver.java
@@ -6,6 +6,8 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
